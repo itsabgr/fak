67e77f7b2252e4bcc555ab773a3b46619f36d1a7
itsabgr-raika <bagheri@raikaco.org>
2024-08-27 11:04:36 +0330

    Create sig.go

---
+++ sig.go
@@ -0,0 +1,22 @@
+func SignalContext(ctx context.Context, signals ...os.Signal) context.Context {
+	if len(signals) <= 0 {
+		panic(errors.New("empty signals"))
+	}
+	var cancelGlobalContext context.CancelFunc
+	globalContext, cancelGlobalContext := context.WithCancel(ctx)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		signalChan := make(chan os.Signal, len(signals)+1)
+		defer fak.Flush(signalChan)
+		defer cancelGlobalContext()
+		signal.Notify(signalChan, signals...)
+		wg.Done()
+		select {
+		case <-signalChan:
+		case <-globalContext.Done():
+		}
+	}()
+	wg.Wait()
+	return globalContext
+}
