42ea174a2ccac5b41c468da8eaad706ce9ed678f
itsabgr-raika <bagheri@raikaco.org>
2024-08-31 13:07:39 +0330

    Update sync.go

--- sync.go
+++ sync.go
@@ -1,5 +1,7 @@
 package fak
 
+import "context"
+
 func LockContext(ctx context.Context, lock interface{ TryLock() bool }) error {
 	for lock.TryLock() == false {
 		if err := ctx.Err(); err != nil {
