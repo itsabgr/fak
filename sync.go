9e34ddcec5f9d14f7ebfe425048ab45f9aae33b0
itsabgr-raika <bagheri@raikaco.org>
2024-08-31 13:32:09 +0330

    Update sync.go

--- sync.go
+++ sync.go
@@ -14,3 +14,18 @@ func LockContext(ctx context.Context, lock interface{ TryLock() bool }) error {
 	}
 	return nil
 }
+
+func Async[R any](ctx context.Context, fn func() (R, error)) (r R, err error) {
+	done := make(chan struct{}, 1)
+	go func() {
+		r, err = fn()
+		done <- struct{}{}
+	}()
+	select {
+	case <-ctx.Done():
+		var zero R
+		return zero, ctx.Err()
+	case <-done:
+		return r, err
+	}
+}
