c7ff64a612610b306891b510bf5455f08d78d63e
itsabgr-raika <bagheri@raikaco.org>
2024-08-31 13:03:56 +0330

    Create sync.go

---
+++ sync.go
@@ -0,0 +1,11 @@
+package fak
+
+func LockContext(ctx context.Context, lock interface{ TryLock() bool }) error {
+	for lock.TryLock() == false {
+		if err := ctx.Err(); err != nil {
+			return err
+		}
+		runtime.Gosched()
+	}
+	return nil
+}
