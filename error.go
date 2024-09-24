4a19e0da8a56f14b192a945268af486c6eeca50b
itsabgr-raika <bagheri@raikaco.org>
2024-09-24 11:29:36 +0330

    Update error.go

--- error.go
+++ error.go
@@ -1,6 +1,33 @@
 package fak
 
 import "errors"
+import "strings"
+
+func ConcatErrors(errs ...error) string {
+	builder := strings.Builder{}
+	for i, err := range errs {
+		if i > 0 {
+			builder.WriteString(": ")
+		}
+		builder.WriteString(err.Error())
+	}
+	return builder.String()
+}
+
+func UnwrapAll(err error) []error {
+	if err == nil {
+		return nil
+	}
+	errs := []error{err}
+	for {
+		uErr := errors.Unwrap(err)
+		if uErr == nil {
+			return errs
+		}
+		errs = append(errs, uErr)
+		err = uErr
+	}
+}
 
 func Throw(err any) {
 	if err != nil {
