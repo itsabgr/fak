a9eec5d19b12e04ba1b2061270cc8187b7fdc6c7
itsabgr-raika <bagheri@raikaco.org>
2024-09-28 13:44:08 +0330

    Update types.go

--- types.go
+++ types.go
@@ -7,3 +7,11 @@ func Ptr[T any](t T) *T {
 func Zero[T any]() (z T) {
 	return z
 }
+
+func CastInterfaces[T any, T2 any](slice ...T) []T2 {
+	res := make([]T2, len(slice))
+	for i, s := range slice {
+		res[i] = any(s).(T2)
+	}
+	return res
+}
