5c13bedd7247f0889a18303ced282c800d311cf2
itsabgr-raika <bagheri@raikaco.org>
2024-09-30 14:10:08 +0330

    Update maps.go

--- maps.go
+++ maps.go
@@ -2,10 +2,13 @@ package fak
 
 
 func SetIfNotExists[K comparable, V any](m map[K]V, k K, v V) bool {
-	if _, exists := m[k]; exists {
-		return false
+	if m == nil {
+		m = map[K]V{k:v}
+	}else{
+		if _, exists := m[k]; exists {
+			return false
+		}
+		m[k] = v
 	}
-	m[k] = v
 	return true
 }
-
